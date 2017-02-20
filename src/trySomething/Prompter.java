1cc524c52edc31e6a5a9d2805f161d60f348d0be
BENJAMIN JOHNSON <wibajohnson@gmail.com>
2017-02-19 19:08:05 -0600

    added guess

---
+++ src/trySomething/Prompter.java
@@ -0,0 +1,16 @@
+package trySomething;
+import java.util.Scanner;
+public class Prompter {
+private Game game;
+
+public Prompter(Game game){
+	this.game = game;
+}
+public boolean promptForGuess(){
+	Scanner scanner = new Scanner(System.in);
+	System.out.print("Enter a Letter: ");
+	String guessInput = scanner.nextLine();
+	char guess = guessInput.charAt(0);
+	return game.applyGuess(guess);
+}
+}
---
+++ src/trySomething/TrySomething.java
@@ -0,0 +1,16 @@
+package trySomething;
+public class TrySomething {
+
+	public static void main(String[] args) {
+		Game game = new Game("treehouse");
+		Prompter prompter = new Prompter(game);
+		boolean isHit = prompter.promptForGuess();
+		if (isHit){
+			System.out.print("Hit");
+		}
+		else{
+			System.out.print("miss");
+		}
+	}
+
+}
