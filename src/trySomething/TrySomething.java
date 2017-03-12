4ae11b5a5cb764cb41bddfccaba3f649be34e94c
BENJAMIN JOHNSON <wibajohnson@gmail.com>
2017-03-11 23:34:18 -0600

    allow user to input the word to guess

--- src/trySomething/TrySomething.java
+++ src/trySomething/TrySomething.java
@@ -1,13 +1,17 @@
 package trySomething;
+import java.util.Scanner;
 public class TrySomething {
 
 	public static void main(String[] args) {
-		if(args.length == 0){
-			System.out.println("Usage: java Hangman <answer>");
-			System.err.println("answer is required");
-			System.exit(1);
-		}
-		Game game = new Game(args[0]);
+	Scanner guessWord = new Scanner(System.in);
+	System.out.println("Enter a word");
+	String word = guessWord.nextLine();
+		Game game = new Game(word);
+//		if(args.length == 0){
+//			System.out.println("Usage: java Hangman <answer>");
+//			System.err.println("answer is required");
+//			System.exit(1);
+//		}
 		Prompter prompter = new Prompter(game);
 		while(game.getRemainingTries() > 0 && !game.isWon()){
 		prompter.displayProgress();
