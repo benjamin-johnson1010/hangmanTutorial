aaf4a786b317bbc786b212bf9b0f03e108877cd6
BENJAMIN JOHNSON <wibajohnson@gmail.com>
2017-03-12 23:31:35 -0500

    new commit

--- src/trySomething/TrySomething.java
+++ src/trySomething/TrySomething.java
@@ -7,19 +7,6 @@ public class TrySomething {
 	System.out.println("Enter a word");
 	String word = guessWord.nextLine();
 		Game game = new Game(word);
-		try{
-			System.out.println("in try");
-		Runtime.getRuntime().exec("clear");
-		}
-		  catch (final Exception e)
-	    {
-			  
-	    }
-//		if(args.length == 0){
-//			System.out.println("Usage: java Hangman <answer>");
-//			System.err.println("answer is required");
-//			System.exit(1);
-//		}
 		Prompter prompter = new Prompter(game);
 		while(game.getRemainingTries() > 0 && !game.isWon()){
 		prompter.displayProgress();
